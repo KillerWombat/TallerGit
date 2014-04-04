dcfb08e359dac1bfaa58c76cec341afc866a1f85
KillerWombat <plarac@outlook.es>
2014-04-04 10:53:45 -0400

    Update Main.java

--- TallerGit/src/cl/taller/git/Main.java
+++ TallerGit/src/cl/taller/git/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("segundo atanganaaaaaaaaaaaaa");
-		System.out.println("linea agregadaaaaa atanganaaa");
+		System.out.println("modificación linea agregadaaaaa atanganaaa");
 	}
 
 }
